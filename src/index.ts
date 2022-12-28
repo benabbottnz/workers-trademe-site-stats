c81fce4d33404b7d58d440a64e9ed16f9f051af2
Ben Abbott <ben@benabbott.nz>
2022-12-28 22:22:37 +1300

    add EditorConfig and apply

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,47 @@
 export interface Env {
-	DB: D1Database;
+  DB: D1Database;
 }
 
 interface SiteStats {
-	MembersOnline: number;
-	ActiveMembers: number;
-	ActiveListings: number;
-	/** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
-	AverageTimeSpent: string;
-	/** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
-	AverageVisitorsPerDay: number;
-	/** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
-	BusiestDayOfWeek: string;
-	/** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
-	ReportMonthAndYear: string;
-	/** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
-	ReportSource: string;
+  MembersOnline: number;
+  ActiveMembers: number;
+  ActiveListings: number;
+  /** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
+  AverageTimeSpent: string;
+  /** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
+  AverageVisitorsPerDay: number;
+  /** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
+  BusiestDayOfWeek: string;
+  /** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
+  ReportMonthAndYear: string;
+  /** @deprecated https://developer.trademe.co.nz/api-reference/catalogue-methods/retrieve-site-statistics **/
+  ReportSource: string;
 }
 
 export default {
-	async scheduled(
-		controller: ScheduledController,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<void> {
-		await fetch('https://api.trademe.co.nz/v1/SiteStats.json')
-			.then(value => value.json<SiteStats>())
-			.then(siteStats => {
-				const unixTimestamp = Math.floor(controller.scheduledTime / 1000);
+  async scheduled(
+    controller: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<void> {
+    await fetch('https://api.trademe.co.nz/v1/SiteStats.json')
+      .then(value => value.json<SiteStats>())
+      .then(siteStats => {
+        const unixTimestamp = Math.floor(controller.scheduledTime / 1000);
 
-				env.DB.prepare("INSERT INTO site_stats (created_at, members_online, active_members, active_listings) VALUES (?1, ?2, ?3, ?4)")
-					.bind(unixTimestamp, siteStats.MembersOnline, siteStats.ActiveMembers, siteStats.ActiveListings)
-					.run()
-			})
-	},
+        env.DB.prepare("INSERT INTO site_stats (created_at, members_online, active_members, active_listings) VALUES (?1, ?2, ?3, ?4)")
+          .bind(unixTimestamp, siteStats.MembersOnline, siteStats.ActiveMembers, siteStats.ActiveListings)
+          .run()
+      })
+  },
 
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const { results } = await env.DB.prepare("SELECT * FROM site_stats").all<SiteStats>()
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const {results} = await env.DB.prepare("SELECT * FROM site_stats").all<SiteStats>()
 
-		return Response.json(results);
-	},
+    return Response.json(results);
+  },
 };
